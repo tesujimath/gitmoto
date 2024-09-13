1c47d8978ce36bde1a4f5c8062ea935a3e5ebae8
Simon Guest <simon.guest@tesujimath.org>
2024-09-13 14:34:53 +1200

    Prettier list of repos

--- src/ui.rs
+++ src/ui.rs
@@ -1,33 +1,29 @@
 use ratatui::{
-    layout::Alignment,
-    style::{Color, Style},
-    widgets::{Block, BorderType, List, Paragraph},
+    layout::{Alignment, Constraint, Layout},
+    widgets::{Block, BorderType, List},
     Frame,
 };
 
 use crate::model::Model;
 
-/// Renders the user interface widgets.
 pub fn render(model: &mut Model, frame: &mut Frame) {
-    // This is where you add new widgets.
-    // See the following resources:
-    // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
-    // - https://github.com/ratatui/ratatui/tree/master/examples
+    const DEFAULT_PATH_LEN: u16 = 20u16;
+    let max_len = model
+        .repos
+        .iter()
+        .map(|repo| repo.path.as_os_str().len() as u16)
+        .max()
+        .unwrap_or(DEFAULT_PATH_LEN);
+    let layout =
+        Layout::horizontal([Constraint::Length(max_len), Constraint::Fill(1)]).split(frame.area());
+
     frame.render_widget(
-        List::new(model.repos.iter().map(|repo| repo.path.to_string_lossy())),
-        // Paragraph::new(
-        //     "This is a work-in-progress.\n\
-        //         Press `Esc`, `Ctrl-C` or `q` to stop running."
-        //         .to_string(),
-        // )
-        // .block(
-        //     Block::bordered()
-        //         .title("Template")
-        //         .title_alignment(Alignment::Center)
-        //         .border_type(BorderType::Rounded),
-        // )
-        // .style(Style::default().fg(Color::Cyan).bg(Color::Black))
-        // .centered(),
-        frame.area(),
+        List::new(model.repos.iter().map(|repo| repo.path.to_string_lossy())).block(
+            Block::bordered()
+                .title(format!("{} local repos", model.repos.len()))
+                .title_alignment(Alignment::Center)
+                .border_type(BorderType::Rounded),
+        ),
+        layout[0],
     )
 }
